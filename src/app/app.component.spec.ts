1ce0da9a870ca37ff2dd691886aab3f8924faf01
Wataru.Kasahara <kasahara1019@gmail.com>
2018-05-27 14:24:20 +0900

    [Issue#24] アプリタイトル以外の不要な要素削除に伴うテストの修正

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Tour of Heroes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
 });
