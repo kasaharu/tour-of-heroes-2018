25e073735217d1527c4676f2f1ae1dc14da86b74
Wataru.Kasahara <kasahara1019@gmail.com>
2018-05-27 16:44:12 +0900

    [Issue#26] HeroesComponent 表示に伴うテストの修正

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -9,6 +11,7 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
+        HeroesComponent,
       ],
     }).compileComponents();
   }));
