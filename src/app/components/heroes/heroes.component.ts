b420656bb66df1ff074b3185442a896302f0cdb9
Wataru.Kasahara <kasahara1019@gmail.com>
2018-05-27 16:30:52 +0900

    [Issue#26] HeroesComponent に hero プロパティを定義

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
+  hero = 'Windstorm';
 
   constructor() { }
 
