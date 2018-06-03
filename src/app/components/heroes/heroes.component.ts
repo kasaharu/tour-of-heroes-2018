6cb18f3a908dfc79ba62f149ae2ef887076af41a
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-03 13:20:21 +0900

    [Issue#37] クリックのイベントハンドラを追加

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -15,10 +15,14 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm',
   };
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
