0e3db955e0a7a98a478bb2feca51ae07ca6adbdc
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-04 22:58:21 +0900

    [Issue#39] HeroDetailComponent を HeroesComponent のテストでインポート

--- src/app/components/heroes/heroes.component.spec.ts
+++ src/app/components/heroes/heroes.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
@@ -10,7 +11,7 @@ describe('HeroesComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
-      declarations: [HeroesComponent],
+      declarations: [HeroesComponent, HeroDetailComponent],
     })
     .compileComponents();
   }));
