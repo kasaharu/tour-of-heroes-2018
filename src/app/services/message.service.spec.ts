3aa3a300dbee6e1c3be1686c3ba7404632b933f2
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-10 17:08:24 +0900

    [Issue#40] MessageService のテストを実装

--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -12,4 +12,16 @@ describe('MessageService', () => {
   it('should be created', inject([MessageService], (service: MessageService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('call the add method', inject([MessageService], (service: MessageService) => {
+    service.messages = [];
+    service.add('test');
+    expect(service.messages).toEqual(['test']);
+  }));
+
+  it('call the clear method', inject([MessageService], (service: MessageService) => {
+    service.messages = ['hoge'];
+    service.clear();
+    expect(service.messages).toEqual([]);
+  }));
 });
