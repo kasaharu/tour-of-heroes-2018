1cd1c41fae7abe4fe79ab3af573bc8da656dfa1e
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-10 16:02:32 +0900

    [Issue#40] Fix lint error: Missing trailing comma

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
 
