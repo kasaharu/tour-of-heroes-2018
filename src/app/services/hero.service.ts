1d0cb1d1e942b2b8b4e0f9ebcb564f41e74ac8e4
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-10 14:06:51 +0900

    [Issue#40] HeroService 内で hero 一覧を取得するメソッドを定義

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from '../models/hero';
+import { HEROES } from '../mocks/mock-heroes';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
