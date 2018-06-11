8196400879dddfc7bc51a39169d896298874d7c8
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-11 13:25:02 +0900

    [Issue#41] HeroesComponent をルーターにセット

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
