147fea45295fe77d793d3b55488f44d5a042bf3c
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-17 17:23:49 +0900

    [Issue#41] DashboardComponent へのルーティングを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
