006b39bd8c9ec14148a642b8e7f99757f75fe36d
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-17 17:47:30 +0900

    [Issue#41] デフォルトルーティングを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
