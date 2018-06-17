b942b5faf1bf7610f55f78a83d29cdb2e8be5f99
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-18 07:13:27 +0900

    [Issue#41] ActiveRoute のテストのために RouterTestingModule を import

--- src/app/components/hero-detail/hero-detail.component.spec.ts
+++ src/app/components/hero-detail/hero-detail.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeroDetailComponent } from './hero-detail.component';
 
@@ -9,7 +10,7 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule],
+      imports: [FormsModule, RouterTestingModule],
       declarations: [ HeroDetailComponent ],
     })
     .compileComponents();
