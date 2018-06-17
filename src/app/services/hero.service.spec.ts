e4b1b80ed62c3582caca1e6da663b225656446c2
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-17 20:57:42 +0900

    [Issue#41] HeroService#getHero の実装とテストの作成

--- src/app/services/hero.service.spec.ts
+++ src/app/services/hero.service.spec.ts
@@ -12,4 +12,10 @@ describe('HeroService', () => {
   it('should be created', inject([HeroService], (service: HeroService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('call the getHero method', inject([HeroService], (service: HeroService) => {
+    service.getHero(11).subscribe(hero => {
+      expect(hero).toEqual({ id: 11, name: 'Mr. Nice' });
+    });
+  }));
 });
