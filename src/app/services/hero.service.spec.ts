5660496d96927cb4ba39c975d119b97d0d21fe6b
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-26 19:32:21 +0900

    [Issue#42] HeroServiceSpec で HttpClientTestingModule をインポート

--- src/app/services/hero.service.spec.ts
+++ src/app/services/hero.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [HeroService],
     });
   });
